db4a7fed9b4cb8abbb2e85bce63b7b031742732e
Brian Sotalin <dannysotalin@gmail.com>
2022-01-23 20:28:46 -0500

    complete

---
+++ data/data.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+      question: "Cual de las siguientes opciones no es un editor de codigo?",
+      choices: [
+      "vim", 
+      "vscode", 
+      "netbeans", 
+      "excel"
+    ],
+      answer: "excel",
+    },
+    {
+      question: "Que lenguaje no es orientado a Objetos?",
+      choices: [
+    "Java", 
+      "Laravel",  
+      "Python"
+    ],
+      answer: "Laravel",
+    },
+    {
+      question: "Que lenguaje no sirve para estilizar sitios web?",
+      choices: [
+      "Stylus",  
+      "Sass", 
+      "CSS", 
+      'PostCSS', 
+      'NextCSS'
+    ],
+      answer: "NextCSS",
+    },
+  ];
